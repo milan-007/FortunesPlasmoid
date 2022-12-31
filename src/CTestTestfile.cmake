d73535ff36cbdb45af0785a4cefde9c5f6ea653f
milan-007 <31536547+milan-007@users.noreply.github.com>
2022-12-31 20:44:58 +0100

    Delete CTestTestfile.cmake

--- src/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/milan/projects/FortunePokus/src
-# Build directory: /home/milan/projects/FortunePokus/src
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("plugin")
